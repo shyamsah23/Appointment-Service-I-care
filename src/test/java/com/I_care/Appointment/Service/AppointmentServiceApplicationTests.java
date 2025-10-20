1f8d25396430ae465784759cb3f276c06a52e8c7
shyamsah23 <92447257+shyamsah23@users.noreply.github.com>
2025-10-20 12:09:50 +0530

    Appointment Service

---
+++ src/test/java/com/I_care/Appointment/Service/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.I_care.Appointment.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
